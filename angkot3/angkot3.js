9a60c9c77cbcecededf06caf4f0b95bddfd8a344
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-02-22 20:26:01 +0700

    mengubah kondisi terminasi pada for agar fleksibel.

--- angkot3/angkot3.js
+++ angkot3/angkot3.js
@@ -2,7 +2,7 @@ var noAngkot = 1;
 var angkotBeroperasi = 6;
 var jumlahAngkot = 10;
 
-for(noAngkot = 1; noAngkot <= 10; noAngkot++){
+for(noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
     if(noAngkot <= angkotBeroperasi){
         console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
     }
