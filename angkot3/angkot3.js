f352b8f2382b2ad8c099a178d4ece6b93dcbbe53
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-02-22 13:30:37 +0700

    membuat file angkot 3 js dan mengaplikasikan perulangan for.

---
+++ angkot3/angkot3.js
@@ -0,0 +1,12 @@
+var noAngkot = 1;
+var angkotBeroperasi = 6;
+var jumlahAngkot = 10;
+
+for(noAngkot = 1; noAngkot <= 10; noAngkot++){
+    if(noAngkot <= 6){
+        console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
+    }
+    else{
+        console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi')
+    }
+}
