f976f8001ffa6819e7cd08bbcf1230e5b3c5c16c
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-02-22 20:19:59 +0700

    memperbaiki syntax kondisi pada if menjadi noAngkot <= angkotBeroperasi , agar fleksibel.

--- angkot3/angkot3.js
+++ angkot3/angkot3.js
@@ -3,7 +3,7 @@ var angkotBeroperasi = 6;
 var jumlahAngkot = 10;
 
 for(noAngkot = 1; noAngkot <= 10; noAngkot++){
-    if(noAngkot <= 6){
+    if(noAngkot <= angkotBeroperasi){
         console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
     }
     else{
