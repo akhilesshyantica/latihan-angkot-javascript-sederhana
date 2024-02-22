842c2333ab237a337d28d15050d5e4895fd1f727
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-02-22 12:22:36 +0700

    perulangan for

---
+++ angkot2/angkot2.js
@@ -0,0 +1,18 @@
+// for(noAngkot = 1; noAngkot <= 6; noAngkot++){
+    
+// }
+// for(noAngkot = 7; noAngkot <=10; noAngkot++){
+//     console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+// }
+
+var noAngkot = 1;
+var jumlahAngkot = 10;
+var angkotBeroperasi = 6;
+
+while(noAngkot <= angkotBeroperasi){
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+    noAngkot++
+}
+for(noAngkot = 7; noAngkot <= jumlahAngkot; noAngkot++){
+    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+}
