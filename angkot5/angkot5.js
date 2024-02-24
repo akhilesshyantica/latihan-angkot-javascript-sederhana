deed1bb469d9389271dfa80a5aacdbf2c0ec9dde
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-02-24 10:21:33 +0700

    commit pertama untuk angkot5.js

---
+++ angkot5/angkot5.js
@@ -0,0 +1,18 @@
+var noAngkot = 1;
+var angkotBeroperasi = 6;
+var jumlahAngkot = 10;
+
+for(noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
+    if(noAngkot <= angkotBeroperasi){
+        console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
+    }
+    else if(noAngkot === 8){
+        console.log('Angkot No. ' + noAngkot + 'sedang lembur.');
+    }
+    else if(noAngkot === 10){
+        console.log('Angkot No. ' + noAngkot + 'sedang lembur.');
+    }
+    else{
+        console.log('Angkot No. ' + noAngkot + ' sedang tidak dapat beroperasi');
+    }
+}
