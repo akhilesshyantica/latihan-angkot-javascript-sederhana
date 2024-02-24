59c5f39358f159083592279216e1367550cddbf4
akhilesshyantica <akhilesshyanticaw@gmail.com>
2024-02-24 10:25:59 +0700

    menambahkan kondisi pada else if untuk angkot no 10.

--- angkot5/angkot5.js
+++ angkot5/angkot5.js
@@ -6,11 +6,8 @@ for(noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
     if(noAngkot <= angkotBeroperasi){
         console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik');
     }
-    else if(noAngkot === 8){
-        console.log('Angkot No. ' + noAngkot + 'sedang lembur.');
-    }
-    else if(noAngkot === 10){
-        console.log('Angkot No. ' + noAngkot + 'sedang lembur.');
+    else if(noAngkot === 8 || noAngkot ===10){
+        console.log('Angkot No. ' + noAngkot + ' sedang lembur.');
     }
     else{
         console.log('Angkot No. ' + noAngkot + ' sedang tidak dapat beroperasi');
